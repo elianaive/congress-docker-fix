unitedstates/congress

commit 83d18c468dabce76e37c5297f4acb5886869aa1d

normalize type and assign 'passage' category to 'On the Motion (Motion to Recede from the Senate Amendment to ____ and Concur with Further Amendment)'

tasks/vote_info.py
@@ -485,6 +485,8 @@ def normalize_vote_type(vote_type):
         (r"On (the )?Nomination", "On the Nomination"),
         (r"On Passage( of the Bill|$)", "On Passage of the Bill"),
         (r"On (the )?Motion to Proceed", "On the Motion to Proceed"),
+        (r"On the Motion \(Motion to ((Recede )from the Senate Amendment to \S+ \d+ (and ))?Concur( with Further Amendment)?", "On the Motion to $2$3Concur$4"),
+        (r"On the Motion \(Motion to (.*)\)$", "On the Motion to $1"),
     )
 
     for regex, replacement in mapping:
@@ -524,6 +526,7 @@ def get_vote_category(vote_question):
         (r"^Guilty or Not Guilty", "conviction"),  # was "impeachment" in sunlightlabs/congress but that's not quite right
         (r"^On (?:the )?Motion to Recommit", "recommit"),
         (r"^On the Motion \(Motion to Concur", "passage"),
+        (r"^On the Motion to Recede and Concur with Further Amendment", "passage"), # this is a normalized type that is returned by a function above
 
         # house only
         (r"^(On Motion (to|that the House) )?(Concur in|Concurring|Concurring in|On Concurring|On Concurring in|Agree to|On Agreeing to) (the )?Senate (Amendment|amdt|Adt)s?", "passage"),

elianaive/congress-docker-fix

commit 0037e924c7fe51bb818121b700934e7bcb9087d8

83d18c468dabce76e37c5297f4acb5886869aa1d broke 'On the Motion to Concur'

tasks/vote_info.py
@@ -525,7 +525,7 @@ def get_vote_category(vote_question):
         (r"^On the Nomination", "nomination"),
         (r"^Guilty or Not Guilty", "conviction"),  # was "impeachment" in sunlightlabs/congress but that's not quite right
         (r"^On (?:the )?Motion to Recommit", "recommit"),
-        (r"^On the Motion \(Motion to Concur", "passage"),
+        (r"^On the Motion to Concur", "passage"),
         (r"^On the Motion to Recede and Concur with Further Amendment", "passage"), # this is a normalized type that is returned by a function above
 
         # house only